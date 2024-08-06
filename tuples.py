35a1216dcb80189227720bc4df0394c9898616d1
Valery Hoyos <73263344+valery-22@users.noreply.github.com>
2024-08-06 11:54:30 -0500

    first commit

---
+++ tuples.py
@@ -0,0 +1,5 @@
+cities_to_visit = ("Barcelona","Amsterdam","Prague","Berlin","Zurich")
+
+print("First city to visit:", cities_to_visit[0])
+
+print("Last city to visit:", cities_to_visit[-1])
---
+++ tuples2.py
@@ -0,0 +1,6 @@
+# TODO: Create a new tuple of cities
+new_cities_visited = ("Bangkok","Dubai","Sydney","Toronto","Cape Town")
+
+# TODO: Access elements in the tuple and print them
+print("First city visited: ", new_cities_visited[0])
+print("Last city visited: ", new_cities_visited[-1])
